1770a92051353e2520a8324f76c57a2ff67f7598
Utkarsh Singh Tomar <dev.utkarshtomar@gmail.com>
2026-02-01 19:54:26 +0530

    improved dropdown links

--- src/components/Header/Dropdown.jsx
+++ src/components/Header/Dropdown.jsx
@@ -1,3 +1,5 @@
+import { FiChevronDown, FiChevronRight } from "react-icons/fi";
+
 const Dropdown = (props) => {
   const { link, depthLevel } = props;
   const hasChild = link.child && link.child.length > 0 ? true : false;
@@ -6,7 +8,10 @@ const Dropdown = (props) => {
     <li className="nav__link">
       {hasChild ? (
         <>
-          <a>{link.label}</a>
+          <a className="d-flex align-items-center gap-1">
+            {link.label}
+            {depthLevel === 0 ? <FiChevronDown /> : <FiChevronRight />}{" "}
+          </a>
           <ul className={`${depthLevel > 0 ? "submenu" : "dropdown__menu"}`}>
             {link.child.map((child, index) => (
               <Dropdown key={index} link={child} depthLevel={depthLevel + 1} />
