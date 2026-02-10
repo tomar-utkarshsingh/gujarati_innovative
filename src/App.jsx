ea9a4d3be2bda7647e927e13a311c8b3d46db639
Utkarsh Singh Tomar <dev.utkarshtomar@gmail.com>
2026-02-10 22:59:55 +0530

    unknown commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import logo1 from "./assets/Images/logo_1.webp";
+import logo2 from "./assets/Images/logo_2.webp";
+
 const App = () => {
   return (
     <>
@@ -5,10 +8,33 @@ const App = () => {
       <main>
         <section className="hero">
           <div className="container">
-            <h1 className="hero__title">
-              Shri Jayantilal Hirachand Sanghvi Gujarati Innovative College of
-              Commerce and Science
-            </h1>
+            <div className="hero__logos">
+              <div className="hero__logo-1">
+                <img
+                  className="img-fluid"
+                  src={logo1}
+                  alt=""
+                  width={150}
+                  height={149}
+                />
+              </div>
+              <div className="hero__logo-2">
+                <img
+                  className="img-fluid"
+                  src={logo2}
+                  alt=""
+                  width={261}
+                  height={175}
+                />
+              </div>
+            </div>
+            <div className="hero__text">
+              <h1 className="hero__title">Welcome To Our Campus</h1>
+              <h2 className="hero__subtitle">
+                A place where career meets guidance and weakness becomes
+                strength
+              </h2>
+            </div>
           </div>
         </section>
       </main>
